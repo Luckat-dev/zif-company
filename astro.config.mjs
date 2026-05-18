7566755e4003dae535620a27b82242161cec9530
Luckat-dev <luckatagondwa6@gmail.com>
2026-05-18 05:49:38 +0200

    Passage en mode static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,5 @@
 import { defineConfig } from 'astro/config';
-<<<<<<< HEAD
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  vite: {
-    define: {
-      'process.env.PUBLIC_SUPABASE_URL': JSON.stringify(process.env.PUBLIC_SUPABASE_URL),
-      'process.env.PUBLIC_SUPABASE_ANON_KEY': JSON.stringify(process.env.PUBLIC_SUPABASE_ANON_KEY),
-    }
-  }
-=======
-import netlify from '@astrojs/netlify';
-
-export default defineConfig({
-  output: 'server',
-  adapter: netlify({
-    imageService: false  // ← Ajoute cette ligne
-  }),
->>>>>>> de44fd0cdbd633a6c9062fdc912a07733cb8395d
+  output: 'static',
 });
